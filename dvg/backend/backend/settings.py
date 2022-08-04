cdd83451b70ca673bbf1858f636c455ad43f7316
Lone Hilke <lonehilke@gmail.com>
2022-08-04 21:37:23 +0200

    prøvet mere step 3
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- dvg/backend/backend/settings.py
+++ dvg/backend/backend/settings.py
@@ -41,9 +41,6 @@ INSTALLED_APPS = [
     'graphene_django',
 ]
 
-GRAPHENE = {
-  "SCHEMA": "blog.schema.schema",
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+GRAPHENE = {
+  "SCHEMA": "blog.schema.schema",
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
